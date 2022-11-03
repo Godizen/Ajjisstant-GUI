bbd0afddcb35b95355fa5c0563fe292a7d2c3d27
Godizen <arnavudupa123@gmail.com>
2022-11-03 22:54:41 +0530

    HD DEMO READY

--- gui.py
+++ gui.py
@@ -52,10 +52,9 @@ class window(Tk):
             exit()
 
         self.servDets()
-        # self.attributes("-fullscreen", True)
-        self.geometry("480x800")
-        self.height = 800  # self.winfo_screenheight()
-        self.width = 480  # self.winfo_screenwidth()
+        self.attributes("-fullscreen", True)
+        self.height = self.winfo_screenheight()
+        self.width = self.winfo_screenwidth()
 
         def updateTime():
             while True:
